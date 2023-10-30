5cbb7d11a2188748b7c410e48082db9a6f3004cd
Ignacio García <ijgarciaest@gmail.com>
2023-10-30 17:53:53 +0100

    arreglo del import de addcharacters

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 import express,{Request,Response} from "npm:express@4.18.2";
 import mongoose from "npm:mongoose@7.6.3";
 import { load } from "https://deno.land/std@0.204.0/dotenv/mod.ts";
-import {addCharacter} from "../Tierra%20Media/funciones/addCharacter.ts"
+import { addCharacter } from "./funciones/addCharacter.ts";
 import { getPersonaje } from "./funciones/getPersonajes.ts";
 const env = await load();
 const MONGO_URL=env.MONGO_URL||Deno.env.get("MONGO_URL")// si hay .emv lo leo si no lo lee de las variables de entorno de deno
