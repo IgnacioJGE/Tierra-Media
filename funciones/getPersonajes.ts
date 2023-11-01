cf22b526c8f513edca3dce61f887795a22113109
Ignacio García <ijgarciaest@gmail.com>
2023-11-01 18:14:52 +0100

    get all y tambien añado el update

--- funciones/getPersonajes.ts
+++ funciones/getPersonajes.ts
@@ -1,26 +1,27 @@
 import { Request, Response } from "npm:express@4.18.2";
-import {ModeloPerson} from "../db/personajes.ts";
+import { ModeloPerson } from "../db/personajes.ts";
 
-
-export const getPersonaje = async (req: Request, res: Response) => {
+const getPersonaje = async (req: Request, res: Response) => {
   try {
-    const { name } = req.params;
-    const personaje = await ModeloPerson.findOne({ name }).exec();
-    if (!personaje) {
-      res.status(404).send("Personaje not found");
+    const persons = await ModeloPerson.find().exec();
+
+    if (!persons) {
+      res.status(404).send("No se encontraron personajes");
       return;
     }
-    res.status(200).send({
-      id: personaje._id.toString(),
-      name: personaje.name,
-      raza: personaje.raza,
-      descripcion: personaje.descripcion,
-      habilidades: personaje.habilidades,
-    });
+
+    const formattedPersons = persons.map((person) => ({
+      name: person.name,
+      raza: person.raza,
+      descripcion: person.descripcion,
+      habilidades: person.habilidades,
+      id: person._id.toString(),
+    }));
+
+    res.status(200).send(formattedPersons);
   } catch (error) {
-    res.status(404).send(error.message);
-    return;
+    res.status(500).send(error.message);
   }
 };
 
-
+export default getPersonaje;
